5286871959a6ab1b469c679f33ca70659cf559de
Faiz <retrocoder123@LAPTOP-B65SACSH>
2024-01-28 09:56:31 +0700

    hi

---
+++ bit++.cpp
@@ -0,0 +1,17 @@
+#include <bits/stdc++.h>
+using namespace std;
+int main () {
+	int n;
+	cin >> n;
+	int x = 0;
+	string statement;
+	for ( int i = 0  ; i < n; i++){
+		cin  >> statement;
+		if (statement.find("++") != string::npos){
+			x++;
+		}else if(statement.find("--") != string::npos){
+			x--;
+		}
+	}
+	cout << x;
+}
