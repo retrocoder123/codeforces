ea267ada6c25f61e80f6d7b34613f2c125c43f1e
Faiz <retrocoder123@LAPTOP-B65SACSH>
2024-02-04 15:41:22 +0700

    fortnite is copying roblox

---
+++ nearly-lucky-number.cpp
@@ -0,0 +1,20 @@
+#include <bits/stdc++.h>
+using namespace std;
+int main () {
+	long n;
+	cin >> n;
+	int luckyNumberCount = 0;
+	while(n > 0){
+		long tens = n % 10;
+		if(tens == 4 || tens == 7) {
+			luckyNumberCount++;
+		}
+		n = n / 10;
+	}
+	if(luckyNumberCount == 4 || luckyNumberCount == 7){
+		cout << "YES";
+	} else {
+		cout << "NO";
+	}
+}
+
